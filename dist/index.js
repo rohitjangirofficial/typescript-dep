e6dd62c398c66209b55376a93faebba84daca60c
Rohit Jangir <rohitjangirofficial@gmail.com>
2024-10-13 21:53:03 +0530

    modified

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+app.get("/", (_req, res) => {
+    return res.send("Express Typescript on Vercel Part 2");
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+app.get("/ping", (_req, res) => {
+    return res.send("pong 🏓");
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
